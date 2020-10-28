03eb934ac5bad134d9c42024001f78663a297459
ItsAnas <97anas@gmail.com>
2020-10-28 18:05:24 +0100

    feat: add goroutine to the program

--- main.go
+++ main.go
@@ -8,9 +8,28 @@ func fibo(n int) int {
 	return fibo(n-1) + fibo(n-2)
 }
 
+func fiboPara(n int, done chan int) {
+	cur := fibo(n)
+	done <- cur
+}
+
 func main() {
-	for i := 0; i < 50; i++ {
+
+	for i := 0; i < 45; i++ {
 		a := fibo(i)
 		println(a)
 	}
+
+	/*
+		done := make(chan int, 45)
+
+		for i := 0; i < 45; i++ {
+			go fiboPara(i, done)
+		}
+
+		for i := 0; i < 45; i++ {
+			res := <-done
+			println(res)
+		}
+	*/
 }
